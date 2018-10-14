27d69fd11609873f2b011a70c5ef0040a1bea984
David McCormick <dmccormick@starrez.com>
2018-10-15 09:44:09 +1100

    Allow console logs

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,32 @@ class App extends React.Component {
         interact('.layout_drag')
             .draggable({
                 onmove: this.dragMoveListener
+            })
+            .resizable({
+                // resize from all edges and corners
+                edges: { left: true, right: true, bottom: true, top: true }
+            })
+            .on('resizemove', (event) => {
+                const target = event.target;
+                let x = (parseFloat(target.getAttribute('data-x')) || 0);
+                let y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+                // update the element's style
+                target.style.width = event.rect.width + 'px';
+                target.style.height = event.rect.height + 'px';
+
+                // translate when resizing from top or left edges
+                x += event.deltaRect.left;
+                y += event.deltaRect.top;
+
+                target.style.webkitTransform = target.style.transform =
+                    'translate(' + x + 'px,' + y + 'px)';
+
+                target.setAttribute('data-x', x);
+                target.setAttribute('data-y', y);
+                target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
             });
+
     }
 
     private dragMoveListener(event) {
@@ -72,17 +97,17 @@ class App extends React.Component {
         // keep the dragged position in the data-x/data-y attributes
         const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
         const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-    
+
         // translate the element
         target.style.webkitTransform =
-        target.style.transform =
-          'translate(' + x + 'px, ' + y + 'px)';
-    
+            target.style.transform =
+            'translate(' + x + 'px, ' + y + 'px)';
+
         // update the posiion attributes
         target.setAttribute('data-x', x);
         target.setAttribute('data-y', y);
-      }
-    
+    }
+
 }
 
 export default App;
